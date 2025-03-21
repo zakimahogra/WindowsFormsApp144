40994aef20cd2d09b48a6793dfea6ad4e378f18b
M S I <M S I@MSI>
2025-03-21 18:31:24 +0700

    untuk mengosongkan semua data yang di input pada textbox dan fokus ke nim agar user siap memasukkan data baru

--- WindowsFormsApp144/Form1.cs
+++ WindowsFormsApp144/Form1.cs
@@ -16,4 +16,26 @@ namespace WindowsFormsApp144
         // Ganti "SERVER" sesuai dengan SQL Server Anda
         private string connectionString = "Data Source=MSI\\ZAKIMAHOGRA;" + "Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
 
-      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Event saat form pertama kali dimuat
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        // Fungsi untuk mengosongkan semua input pada TextBox
+        private void ClearForm()
+        {
+            txtNIM.Clear();
+            txtNama.Clear();
+            txtEmail.Clear();
+            txtTelepon.Clear();
+            txtAlamat.Clear();
+
+            // Fokus kembali ke NIM agar user siap memasukkan data baru
+            txtNIM.Focus();
+        }
