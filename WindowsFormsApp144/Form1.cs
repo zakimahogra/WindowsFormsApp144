f6b1bf8feb2910618301199baf6370a279f33b68
M S I <M S I@MSI>
2025-03-21 18:29:51 +0700

    menyambungkan dengan database do sql server

--- WindowsFormsApp144/Form1.cs
+++ WindowsFormsApp144/Form1.cs
@@ -11,4 +11,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp144
 {
- 
+    public partial class Form1 : Form
+    {
+        // Ganti "SERVER" sesuai dengan SQL Server Anda
+        private string connectionString = "Data Source=MSI\\ZAKIMAHOGRA;" + "Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
+
+      
